51cf8f7e1fdd7a08013143af082bdfda2e9e4257
brandonwc5 <brandonwc5@yahoo.com>
2018-02-01 21:03:12 -0600

    further

--- reservations.js
+++ reservations.js
@@ -79,11 +79,11 @@ app.post("/api/new", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body-parser middleware
   var newReservation = req.body;
-
+  newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
   console.log(newReservation);
 
   // We then add the json the user sent to the character array
-  characters.push(newReservation);
+  reservation.push(newReservation);
 
   // We then display the JSON to the users
   res.json(newReservation);
